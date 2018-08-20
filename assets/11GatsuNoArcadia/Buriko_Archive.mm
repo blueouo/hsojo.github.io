d207b9f6fcfc059b0bdfb2446f70c81ebb986c4a
HsOjo <1134031392@qq.com>
2018-08-20 21:23:38 +0800

    Site updated: 2018-08-20 21:23:37

---
+++ assets/11GatsuNoArcadia/Buriko_Archive.mm
@@ -0,0 +1,70 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1508314936058" ID="ID_760327097" MODIFIED="1508315288119" TEXT="Buriko Archive">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1508315002046" ID="ID_366339700" MODIFIED="1508315288109" POSITION="right" TEXT="File Header">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1508315015657" ID="ID_301079735" MODIFIED="1508315757146" TEXT="(char[12])BURIKO ARC20">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1508315068223" ID="ID_902564643" MODIFIED="1508315288116" POSITION="right" TEXT="File Num">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1508315075472" ID="ID_1647571721" MODIFIED="1508315352957" TEXT="(int)file_num">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1508315301844" ID="ID_751442566" MODIFIED="1508315659015" POSITION="right" TEXT="File Info">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1508315329432" ID="ID_1073093509" MODIFIED="1508315700715" TEXT="(struct[file_num])file_info">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1508315701199" ID="ID_1108888187" MODIFIED="1508315724899" TEXT="(char[96])name">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1508315406684" ID="ID_1753689310" MODIFIED="1508315429392" TEXT="(int)offset">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1508315430402" ID="ID_1680822416" MODIFIED="1508315433196" TEXT="(int)size">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1508315434401" ID="ID_1389599072" MODIFIED="1508315440565" TEXT="(int)unknown">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1508315434401" ID="ID_1939573250" MODIFIED="1508315440565" TEXT="(int)unknown">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1508315434401" ID="ID_555247760" MODIFIED="1508315440565" TEXT="(int)unknown">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1508315434401" ID="ID_63819266" MODIFIED="1508315440565" TEXT="(int)unknown">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1508315434401" ID="ID_1937663563" MODIFIED="1508315481771" TEXT="(int)is_raw_data">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1508315434401" ID="ID_351329645" MODIFIED="1508315440565" TEXT="(int)unknown">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1508315781750" ID="ID_1964358402" MODIFIED="1508315784783" POSITION="right" TEXT="File Data">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1508315839653" ID="ID_1900235564" MODIFIED="1508315886522" TEXT="(struct[file_num])file_data">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1508315784784" ID="ID_94591759" MODIFIED="1508315842741" TEXT="(bytes[size])data">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ assets/11GatsuNoArcadia/Buriko_Script.mm
@@ -0,0 +1,100 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1508310427176" ID="ID_79742685" MODIFIED="1508315233361">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Buriko Script
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1508310439720" ID="ID_619904101" MODIFIED="1508311683446" POSITION="right" TEXT="File Header">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1508310454440" ID="ID_1118402351" MODIFIED="1508311683448" TEXT="(string)magic">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1508310789225" ID="ID_634049005" MODIFIED="1508311683449" TEXT="BurikoCompiledScriptVer1.00">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1508310543848" ID="ID_1118830839" MODIFIED="1508311683451" POSITION="right" TEXT="Package List">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1508310634380" ID="ID_1428366333" MODIFIED="1508311683452" TEXT="(int)length">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1508310652291" ID="ID_900962841" MODIFIED="1508311683453" TEXT="(int)package_count">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1508310666475" ID="ID_1427583555" MODIFIED="1508311683455" TEXT="(string[package_count])package_name">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1508311375466" ID="ID_1917032802" MODIFIED="1508311683457" POSITION="right" TEXT="Command Code">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1508310481220" ID="ID_804704949" MODIFIED="1508311683459" TEXT="head">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1508310944515" ID="ID_1511704306" MODIFIED="1508314781724" TEXT="(int)unknown">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1508311308695" ID="ID_201125655" MODIFIED="1508311683460" TEXT="(int)code_foot_position">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1508311421472" ID="ID_851542654" MODIFIED="1508311683463" TEXT="code_body">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1508315151749" ID="ID_1436847026" MODIFIED="1508315207338" TEXT="(struct[?])command">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1508311486734" ID="ID_633909546" MODIFIED="1508315388875" TEXT="(int)cmd_id">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1508311502085" ID="ID_1837364829" MODIFIED="1508315158806" TEXT="(int[?])param">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1508311421472" ID="ID_242416360" MODIFIED="1508311683469" TEXT="code_foot">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1508315151749" ID="ID_632344638" MODIFIED="1508315209226" TEXT="(struct[?])command">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1508311486734" ID="ID_723144381" MODIFIED="1508315393472" TEXT="(int)cmd_id">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1508311502085" ID="ID_1016634187" MODIFIED="1508315158806" TEXT="(int[?])param">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1508311566592" ID="ID_710101553" MODIFIED="1508311683483" POSITION="right" TEXT="BSB">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1508311611264" ID="ID_1565716383" MODIFIED="1508311683484" TEXT="(string)path">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1508311566592" ID="ID_529519196" MODIFIED="1508311683486" POSITION="right" TEXT="Code Text">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1508311639065" ID="ID_602843743" MODIFIED="1508311683488" TEXT="(string[?])text">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
